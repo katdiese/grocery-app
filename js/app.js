90b34ec993cf9267865fa521b4308638328a52e0
Kathy Dieser <katdiese@gmail.com>
2015-04-29 20:18:33 -0600

    Added images and interactive elements

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  
+//add new items to the list with the input box
+  $(".add").click(function(){
+    $(".items ul").prepend(
+      $('<li><div class="box"></div>' + $("input").val() + '<div class="delete"></div></li>')
+    );
+  });
+  
+//check box/strike through text when clicked
+  $('ul').on('click','li',function(event){
+    $(this).toggleClass('two');
+    $(this).children('.box').toggleClass('checked');
+  })
+  
+//show delete sign on hover
+  $('ul').on('mouseenter','li',function(){
+    $(this).children('.delete').css('display','inline');
+  });
+  $('ul').on('mouseleave','li',function(){
+    $(this).children('.delete').hide();
+  });
+
+//remove when delete is pressed
+  $('ul').on('click', '.delete', function(e){
+    e.preventDefault();
+    $(this).parent().remove();
+  });
+    
+ 
+});
